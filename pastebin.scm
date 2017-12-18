27a5ed4f5ee370fe4162841ac2bafadc37beb849
Jelle Licht <jlicht@fsfe.org>
2017-12-18 01:14:09 +0100

    Indention fix-up of `pastebin.scm'.

--- pastebin.scm
+++ pastebin.scm
@@ -43,16 +43,16 @@
   (if (string-ci=? charset "utf-8")
       (utf8->string bv)
       (let ((p (open-bytevector-input-port bv)))
-	(set-port-encoding! p charset)
-	(read-delimited "" p))))
+        (set-port-encoding! p charset)
+        (read-delimited "" p))))
 
 (define* (parse-www-form-urlencoded str #:optional (charset "utf-8"))
   (map (lambda (piece)
-	 (let ((equals (string-index piece #\=)))
-	   (if equals
-	       (cons (uri-decode (substring piece 0 equals) #:encoding charset)
-		     (uri-decode (substring piece (1+ equals)) #:encoding charset))
-	       (cons (uri-decode piece #:encoding charset) ""))))
+         (let ((equals (string-index piece #\=)))
+           (if equals
+               (cons (uri-decode (substring piece 0 equals) #:encoding charset)
+                     (uri-decode (substring piece (1+ equals)) #:encoding charset))
+               (cons (uri-decode piece #:encoding charset) ""))))
        (string-split str #\&)))
 
 (define (request-form-data request body)
@@ -63,13 +63,13 @@
       ;; content.
       (request-form-data request (decode-string body "iso-8859-1"))
       (if (or (not body)
-	      (string-null? body))
-	  '()
-	  (let* ((content-type (request-content-type request))
-		 (charset (or (assoc-ref (cdr content-type) "charset") "utf-8")))
-	    (cond
-	     ((equal? (car content-type) 'application/x-www-form-urlencoded)
-	      (parse-www-form-urlencoded body charset)))))))
+              (string-null? body))
+          '()
+          (let* ((content-type (request-content-type request))
+                 (charset (or (assoc-ref (cdr content-type) "charset") "utf-8")))
+            (cond
+             ((equal? (car content-type) 'application/x-www-form-urlencoded)
+              (parse-www-form-urlencoded body charset)))))))
 
 (define (get-request? request)
   (eq? (request-method request) 'GET))
@@ -85,49 +85,49 @@ example: \"/foo/bar\" yields '(\"foo\" \"bar\")."
 (define (make-request-handler)
   (lambda (request body)
     (format #t "~a ~a~%"
-	    (request-method request)
-	    (uri-path (request-uri request)))
+            (request-method request)
+            (uri-path (request-uri request)))
 
     (cond
      ((get-request? request)
       (match (request-path-components request)
-	((or ("index.html") '())
-	 (render-html (template `(div (@ (id "content")
-					 (class "container-fluid"))
-				      (h1 "The index awww yeah")))))
-	(("paste" "new")
-	 (render-html (template paste-form)))
-	(("paste" uid)
-	 (let ((paste (get-paste uid)))
-	   (render-html (template `(div (@ (id "content")
-					   (class "container-fluid"))
-					(h1 ,(paste-name paste))
-          ;; TODO: Do not assume lex-scheme
-					(pre (code ,(highlights->sxml
-                       (highlight lex-scheme (paste-code paste))))))))))
-	(("static" path ...)
-	 (render-static-asset request))
-	(("favicon.ico")
-	 (render-static-asset request))
-	(_
-	 (not-found request))))
+        ((or ("index.html") '())
+         (render-html (template `(div (@ (id "content")
+                                         (class "container-fluid"))
+                                      (h1 "The index awww yeah")))))
+        (("paste" "new")
+         (render-html (template paste-form)))
+        (("paste" uid)
+         (let ((paste (get-paste uid)))
+           (render-html (template `(div (@ (id "content")
+                                           (class "container-fluid"))
+                                        (h1 ,(paste-name paste))
+                                        ;; TODO: Do not assume lex-scheme
+                                        (pre (code ,(highlights->sxml
+                                                     (highlight lex-scheme (paste-code paste))))))))))
+        (("static" path ...)
+         (render-static-asset request))
+        (("favicon.ico")
+         (render-static-asset request))
+        (_
+         (not-found request))))
      ((post-request? request)
       (match (request-path-components request)
-	(("paste" "new")
-	 (if (bytevector? body)
-	     (let* ((data (request-form-data request body))
-		    (name (cdr (car data)))
-		    (code (cdr (car (cdr data))))
-		    (uid (new-paste (make-paste name code))))
-	       (redirect (list "paste" uid)))
-	     (render-html (template (not-found request #:phrase "incorrect form data")))))
-	(_
-	 (not-found request)))))))
+        (("paste" "new")
+         (if (bytevector? body)
+             (let* ((data (request-form-data request body))
+                    (name (cdr (car data)))
+                    (code (cdr (car (cdr data))))
+                    (uid (new-paste (make-paste name code))))
+               (redirect (list "paste" uid)))
+             (render-html (template (not-found request #:phrase "incorrect form data")))))
+        (_
+         (not-found request)))))))
 
 (define* (run-pastebin #:key (repl? #f))
   (when repl?
     (repl:spawn-server (repl:make-tcp-server-socket)))
 
   (with-env paste-env
-    (format #t "Server Started: http://localhost:8080/\n\n")
-    (run-server (make-request-handler))))
+            (format #t "Server Started: http://localhost:8080/\n\n")
+            (run-server (make-request-handler))))
